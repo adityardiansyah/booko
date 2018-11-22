067c00f4c26388f277c089efc97462590b7fadda
MirHmzh <corn.row81@gmail.com>
2018-11-22 21:13:50 +0700

    added animation

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+ScrollReveal().reveal('.book-product',{
+	interval: 200,
+	origin: 'bottom',
+	distance: '10%',
+	// opacity: null,
+});
+ScrollReveal().reveal('#left-banner', {
+	origin: 'left',
+	distance: '150%',
+	easing : 'ease',
+	duration : 1000,
+});
+ScrollReveal().reveal('#right-banner', {
+	origin: 'right',
+	distance: '150%',
+	easing : 'ease',
+	duration : 1000,
+});
